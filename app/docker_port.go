db7bda93b316f0879917753863acc125da3c8ec9
maddalax <jm@madev.me>
2024-11-27 11:42:51 -0600

    randomize port selection

--- app/docker_port.go
+++ app/docker_port.go
@@ -1,18 +1,34 @@
 package app
 
 import (
+	"dockside/app/logger"
 	"fmt"
+	"math/rand"
 	"net"
+	"time"
 )
 
 func FindOpenPort(startPort int) (int, error) {
-	for port := startPort; port <= 65535; port++ {
-		address := fmt.Sprintf(":%d", port)
+	minp := startPort
+	maxp := 65534
+	attempts := 0
+	for {
+		attempts++
+		if attempts > 100 {
+			break
+		}
+		randomPort := rand.Intn(maxp-minp+1) + minp
+		logger.InfoWithFields("Trying to find open port", map[string]any{
+			"port": randomPort,
+		})
+		address := fmt.Sprintf(":%d", randomPort)
 		listener, err := net.Listen("tcp", address)
 		if err == nil {
 			listener.Close()
-			return port, nil
+			return randomPort, nil
 		}
+		time.Sleep(time.Millisecond * 10)
 	}
+
 	return 0, fmt.Errorf("no open ports found starting from %d", startPort)
 }
